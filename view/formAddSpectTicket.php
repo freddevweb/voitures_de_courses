92e5435c0123bf2063139cc16da875a58d59ad8c
fred <fredfredfredm@yahoo.fr>
2017-07-27 18:17:41 +0200

    add somes view and services

--- view/formAddSpectTicket.php
+++ view/formAddSpectTicket.php
@@ -4,10 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Liste des voitures</title>
+    <title>Ajouter un ticket</title>
 </head>
 <body>
-    <h1>Liste des voitures</h1>
+    <h1>Ajouter un ticket</h1>
+    <form>
+    
+    
+    
+    
+    </form>
+
+
 
 
 
--- view/tickets_courses.php
+++ view/tickets_courses.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Liste des voitures</title>
+    <title>Liste des tickets</title>
 </head>
 <body>
-    <h1>Liste des voitures</h1>
+    <h1>Liste des tickets par course</h1>
 
 
 
